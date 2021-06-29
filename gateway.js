ff68e71125841748d73379990d5b0e09675b2ebd
godfreydoo <godfrey.doo@gmail.com>
2021-06-29 15:35:18 -0700

    Modify loader verification route and response

--- gateway.js
+++ gateway.js
@@ -2,12 +2,21 @@ const express = require('express');
 const routes = require('./routes/index');
 const app = express();
 const PORT = 3000;
+const fs = require('fs');
 require('dotenv').config();
 
 
 // For testing
-app.get(`/${process.env.LOADER_PATH}`, (req, res) => {
-  res.end(process.env.LOADER_DATA);
+app.get(`/${process.env.LOADER_FILE}`, async (req, res) => {
+
+  try {
+    const data = fs.readFileSync(`.${process.env.LOADER_FILE}`, 'utf8')
+    res.end(data);
+  } catch (err) {
+    console.error(err);
+    res.end();
+  }
+
 })
 
 
