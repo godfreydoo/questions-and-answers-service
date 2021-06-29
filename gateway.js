e9a27501dcf5105c40dfa38e320830f0b972f50c
godfreydoo <godfrey.doo@gmail.com>
2021-06-29 14:51:13 -0700

    Update data to support cloud load testing

--- gateway.js
+++ gateway.js
@@ -2,6 +2,14 @@ const express = require('express');
 const routes = require('./routes/index');
 const app = express();
 const PORT = 3000;
+require('dotenv').config();
+
+
+// For testing
+app.get(`/${process.env.LOADER_PATH}`, (req, res) => {
+  res.end(process.env.LOADER_DATA);
+})
+
 
 // Middleware
 app.use(express.json());
